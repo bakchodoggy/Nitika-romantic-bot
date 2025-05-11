ac4d0f88293da7de9e41a3b6f2fc892f72137ab1
bakchodoggy <bakchodoggy8@gmail.com>
2025-05-12 01:46:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Start FastAPI API on port 8080 in the background
-uvicorn main:app --host 0.0.0.0 --port 8080 &
+uvicorn stars_payment:app --host 0.0.0.0 --port 8080 &
 # Start your Telegram bot runner
 python3 bot_runner.py
